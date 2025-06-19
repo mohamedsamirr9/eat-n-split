3de4fb3fa196a34dcc7c8b06751a96de27423b4e
mohamedsamirr9 <mohamedsamir979797@gmail.com>
2025-06-19 11:28:23 +0300

    friends list added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialFriends = [
   {
     id: 118836,
@@ -18,3 +20,91 @@ const initialFriends = [
     balance: 0,
   },
 ];
+export default function App() {
+  return (
+    <>
+      <div className="app">
+        <FriendsList />
+      </div>
+    </>
+  );
+}
+
+function FriendsList() {
+  const [friends, setFriends] = useState(initialFriends);
+  const [openAddFriend, setOpenAddFriend] = useState(false);
+  const [name, setName] = useState("");
+  const [url, setUrl] = useState("https://i.pravatar.cc/48");
+
+  function addFriend(e) {
+    e.preventDefault();
+    if (name === "" || url === "") return;
+    setFriends([
+      ...friends,
+      { id: Date.now(), name: name, image: url, balance: 0 },
+    ]);
+    setName("");
+    setOpenAddFriend(false);
+  }
+
+  return (
+    <div className="sidebar">
+      <ul>
+        {friends.map((friend) => (
+          <Friend friend={friend} key={friend.id} />
+        ))}
+      </ul>
+      {openAddFriend && (
+        <AddFriend
+          onAddFriend={addFriend}
+          onSetName={setName}
+          onSetUrl={setUrl}
+          url={url}
+        />
+      )}
+
+      <button onClick={() => setOpenAddFriend(!openAddFriend)} class="button">
+        {openAddFriend ? "Close" : "Add friend"}
+      </button>
+    </div>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+      <p
+        className={`${
+          friend.balance === 0 ? "" : friend.balance > 0 ? "green" : "red"
+        }`}
+      >
+        {friend.balance === 0
+          ? "You and Anthony are even"
+          : friend.balance > 0
+          ? `${friend.name} owes you ${friend.balance}€`
+          : `You owe ${friend.name} ${Math.abs(friend.balance)}€`}
+      </p>
+      <button className="button">Select</button>
+    </li>
+  );
+}
+
+function AddFriend({ onAddFriend, onSetName, onSetUrl, url }) {
+  return (
+    <form onSubmit={(e) => onAddFriend(e)} className="form-add-friend">
+      <label>👫 Friend name</label>
+      <input type="text" onChange={(e) => onSetName(e.target.value)} />
+      <label>🌄 Image URL</label>
+      <input
+        type="text"
+        value={url}
+        onChange={(e) => onSetUrl(e.target.value)}
+      />
+      <button onClick={(e) => onAddFriend(e)} className="button">
+        Add
+      </button>
+    </form>
+  );
+}
